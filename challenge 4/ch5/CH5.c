934582cee41dabe632edf22c1ac2c6968cf1782d
Tifaoute <maachatifaoute@gmail.com>
2024-09-26 15:30:20 +0200

    version

---
+++ challenge 4/ch5/CH5.c
@@ -0,0 +1,29 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{  float a,b;
+   float sum,ad,div,mul;
+
+      printf("Entrer un nombre reel a:");
+      scanf("%f",&a);
+
+      printf("Entrer un nombre reel b:");
+      scanf("%f",&b);
+
+      sum = a+b;
+      printf("a+b :%.2f\n",sum);
+
+      ad = a-b;
+      printf("a-b:%.2f\n",ad);
+
+      mul = a*b;
+      printf("a*b:%.2f\n",mul);
+
+      div = a/b;
+      printf("a/b:%.2f",div);
+
+    return 0;
+}
+*/
